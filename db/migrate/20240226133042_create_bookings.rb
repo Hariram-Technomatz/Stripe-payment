0b319e6b3e84a757ddcbeaae4d03b9b313270e20
Arvind Singh <arvind.b@ksolves.com>
2024-02-27 11:31:48 +0530

    fix workshop in booking

--- db/migrate/20240226133042_create_bookings.rb
+++ db/migrate/20240226133042_create_bookings.rb
@@ -5,7 +5,7 @@ class CreateBookings < ActiveRecord::Migration[7.0]
       t.float :amount_paid
       t.string :stripe_transaction_id
       t.references :customer, null: false, foreign_key: true
-      t.references :warkshop, null: false, foreign_key: true
+      t.references :workshop, null: false, foreign_key: true
 
       t.timestamps
     end
